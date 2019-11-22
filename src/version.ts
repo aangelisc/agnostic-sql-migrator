fd126b4375eb93894467a63565308fad9d2fecf3
aangelisc <andreas.christou@telefonica.com>
2019-11-22 15:50:59 +0000

    Update main code to use new adapter structure

--- src/version.ts
+++ src/version.ts
@@ -1,22 +1,37 @@
-import { Client } from "pg";
+import { AdapterClients, AdapterClient } from ".";
 
 export const Version = {
-  exists: async (client: Client): Promise<boolean> => {
-    const exists = await client.query(
+  exists: async (
+    client: AdapterClients,
+    adapter: AdapterClient
+  ): Promise<boolean> => {
+    const exists = await adapter.query(
+      client,
       "SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_name = 'version') as value;"
     );
     return exists.rows[0].value;
   },
-  create: async (client: Client): Promise<void> => {
-    await client.query(
+  create: async (
+    client: AdapterClients,
+    adapter: AdapterClient
+  ): Promise<void> => {
+    await adapter.query(
+      client,
       "CREATE TABLE version (value INT);INSERT INTO version(value) VALUES(1);"
     );
   },
-  update: async (client: Client, version: number): Promise<void> => {
-    await client.query("UPDATE version SET value=$1", [version]);
+  update: async (
+    client: AdapterClients,
+    adapter: AdapterClient,
+    version: number
+  ): Promise<void> => {
+    await adapter.query(client, `UPDATE version SET value=${version}`);
   },
-  get: async (client: Client): Promise<number> => {
-    const version = await client.query("SELECT value FROM version");
+  get: async (
+    client: AdapterClients,
+    adapter: AdapterClient
+  ): Promise<number> => {
+    const version = await adapter.query(client, "SELECT value FROM version");
     return Number.parseInt(version.rows[0].value);
   }
 };

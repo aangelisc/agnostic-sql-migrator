6a6562b0ede5546878f6134df1928832646e006f
aangelisc <andreas.christou@telefonica.com>
2019-11-25 15:13:07 +0000

    Add further postgres based tests
    - Test db initialisation
    - Test default migration pathway
    - Test db migrations if db is at the latest evrsion
    - Test rollback
    - Test migration to specified version

--- tests/postgres.test.ts
+++ tests/postgres.test.ts
@@ -2,6 +2,9 @@ import * as Containers from "testcontainers";
 import { adapters } from "../src/adapters";
 import { Version } from "../src/version";
 import { Config, AdapterClient } from "../src/config";
+import { getMigrationFiles, migrateDb } from "../src/migrations";
+import { mkdirSync, rmdirSync } from "fs";
+import { join } from "path";
 
 let config: Config;
 let container: Containers.StartedTestContainer;
@@ -46,10 +49,85 @@ describe("Testing Postgres functionality", () => {
     expect(logSpy).toHaveBeenCalledTimes(2);
   });
 
-  it("Will check if the version exists", async () => {
+  it("Will check if the version exists - on initial creation expect this to be false", async () => {
     const client = await adapter.createClient(config);
     const exists = await Version.exists(client, adapter);
-    console.log(exists);
+    expect(exists).toBeFalsy();
+    await adapter.closeConnection(client);
+  });
+
+  it("Will initialise db version at 1 if no version table exists", async () => {
+    const client = await adapter.createClient(config);
+    const exists = await Version.exists(client, adapter);
+    expect(exists).toBeFalsy();
+    await Version.create(client, adapter);
+    expect(logSpy).toHaveBeenCalledWith(
+      "New DB - Creating version information"
+    );
+    const version = await Version.get(client, adapter);
+    expect(version).toEqual(1);
+    expect(logSpy).toHaveBeenLastCalledWith("Current DB Version is: ", 1);
+    await adapter.closeConnection(client);
+  });
+
+  it("Will initialise db and carry out default migration pathway", async () => {
+    const migrationFiles = getMigrationFiles(config);
+    const latest =
+      migrationFiles.RollForward[migrationFiles.RollForward.length - 1]
+        .VersionTo;
+    Object.assign(config, {
+      version: latest
+    });
+    const client = await adapter.createClient(config);
+    await migrateDb(client, adapter, config.version, migrationFiles);
+    expect(logSpy).toHaveBeenCalledWith(
+      "Rolling forwards to version: ",
+      latest
+    );
+    const version = await Version.get(client, adapter);
+    expect(version).toEqual(latest);
+    await adapter.closeConnection(client);
+  });
+
+  it("Will not carry out migrations if db is at latest version", async () => {
+    const migrationFiles = getMigrationFiles(config);
+    const latest =
+      migrationFiles.RollForward[migrationFiles.RollForward.length - 1]
+        .VersionTo;
+    Object.assign(config, {
+      version: latest
+    });
+    const client = await adapter.createClient(config);
+    await migrateDb(client, adapter, config.version, migrationFiles);
+    expect(logSpy).toHaveBeenCalledWith(
+      "DB is already at the specified version - no migrations to carry out."
+    );
+    const version = await Version.get(client, adapter);
+    expect(version).toEqual(latest);
+    await adapter.closeConnection(client);
+  });
+
+  it("Will rollback the db to a specified version", async () => {
+    const migrationFiles = getMigrationFiles(config);
+    Object.assign(config, {
+      version: 1
+    });
+    const client = await adapter.createClient(config);
+    await migrateDb(client, adapter, config.version, migrationFiles);
+    expect(logSpy).toHaveBeenCalledWith("Rolling backwards to version: ", 1);
+    const version = await Version.get(client, adapter);
+    expect(version).toEqual(1);
+    await adapter.closeConnection(client);
+  });
+
+  it("Will initialise db and carry out migration to a specified version", async () => {
+    const migrationFiles = getMigrationFiles(config);
+    Object.assign(config, { version: 3 });
+    const client = await adapter.createClient(config);
+    await migrateDb(client, adapter, config.version, migrationFiles);
+    expect(logSpy).toHaveBeenCalledWith("Rolling forwards to version: ", 3);
+    const version = await Version.get(client, adapter);
+    expect(version).toEqual(3);
     await adapter.closeConnection(client);
   });
 });

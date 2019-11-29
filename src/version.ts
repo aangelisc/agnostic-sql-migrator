005b598b8e63821e6cc38c6b9783c0b03ecef431
aangelisc <andreas.christou@telefonica.com>
2019-11-29 19:53:14 +0000

    Update version functions to account for MySQL

--- src/version.ts
+++ src/version.ts
@@ -9,17 +9,15 @@ export const Version = {
       client,
       "SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_name = 'version') as value;"
     );
-    return exists.rows[0].value;
+    return exists[0].value;
   },
   create: async (
     client: AdapterClients,
     adapter: AdapterClient
   ): Promise<void> => {
     console.log("New DB - Creating version information");
-    await adapter.query(
-      client,
-      "CREATE TABLE version (value INT);INSERT INTO version(value) VALUES(1);"
-    );
+    await adapter.query(client, "CREATE TABLE version (value INT);");
+    await adapter.query(client, `INSERT INTO version (value) VALUES(1);`);
   },
   update: async (
     client: AdapterClients,
@@ -33,7 +31,7 @@ export const Version = {
     adapter: AdapterClient
   ): Promise<number> => {
     const version = await adapter.query(client, "SELECT value FROM version");
-    const currentVersion = Number.parseInt(version.rows[0].value);
+    const currentVersion = Number.parseInt(version[0].value);
     console.log("Current DB Version is: ", currentVersion);
     return currentVersion;
   }

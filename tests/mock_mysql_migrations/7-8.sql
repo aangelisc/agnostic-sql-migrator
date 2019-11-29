7fce8a12992967f9b6a742a8f295302183b8f14d
aangelisc <andreas.christou@telefonica.com>
2019-11-29 19:54:23 +0000

    Add MySQL mock migrations

---
+++ tests/mock_mysql_migrations/7-8.sql
@@ -0,0 +1,8 @@
+CREATE TABLE discounts (
+    onerow_id boolean DEFAULT TRUE PRIMARY KEY,
+    second_discount DECIMAL,
+    third_discount DECIMAL,
+    fourth_discount DECIMAL,
+    fifth_discount DECIMAL,
+    max_discounts INT
+);

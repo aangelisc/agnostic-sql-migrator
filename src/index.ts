f367ba7d93f21fd3cee27b33e91e6da095975294
aangelisc <andreas.christou@telefonica.com>
2020-01-15 23:40:35 +0000

    Attempt to fix npm packaging
    - Ignore dist and ds_store
    - Version bump
    - Update main file and types
    - Add prebuild script
    - Add dist as package files
    - Export files from index
    - Update tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { entrypoint } from "./config";
+export * from "./config";
+export * from "./migrations";
+export * from "./version";
+export * from "./adapters";
 
 entrypoint().catch(err => console.log(err));
